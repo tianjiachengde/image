4c963d1dd7b26934c9fb4f73900e1c88e06d7bdc
tianjiacheng <jiacheng.tian@baozun.com>
2019-04-10 10:00:25 +0800

    git ignore

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-import ss from 'image-screen-shoot-tjc'
-Vue.use(ss);
-new Vue({
-  render: function (h) { return h(App) },
-}).$mount('#app')
