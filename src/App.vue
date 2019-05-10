d93690e08aa0d569e9704b129d74db8e9ccc1054
tianjiacheng <jiacheng.tian@baozun.com>
2019-05-10 16:02:04 +0800

    nothing

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <image-screen-shoot :width="400"
+             :height="400"
+             :url="url"
+             @image-info="handleImageInfo">
+        </image-screen-shoot>
+    </div>
+</template>
+
+<script>
+    import imageComponent from 'image-screen-shoot-tjc'
+    export default {
+        components:{
+            imageScreenShoot:imageComponent.imageComponent,
+        },
+        data() {
+            return {
+                url: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+            };
+        },
+        methods: {
+            handleImageInfo(e) {
+                console.log(e);
+            },
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
